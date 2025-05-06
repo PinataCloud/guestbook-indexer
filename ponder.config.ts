3fc035acfdce46e8a1677df5ddec873bd13632a4
Steve <stevedsimkins@gmail.com>
2025-05-06 19:51:27 -0400

    chore: Updated polling interval

--- ponder.config.ts
+++ ponder.config.ts
@@ -8,7 +8,7 @@ export default createConfig({
     arbitrum: {
       chainId: 42161,
       transport: http(process.env.PONDER_RPC_URL_42161),
- 			pollingInterval: 2_500,
+ 			pollingInterval: 1_500,
     },
   },
   contracts: {
