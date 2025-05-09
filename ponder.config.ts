17cdaf8863fc71dd3884f375386b724922178516
Steve <stevedsimkins@gmail.com>
2025-05-09 13:32:59 -0400

    chore: Updated contract address

--- ponder.config.ts
+++ ponder.config.ts
@@ -4,19 +4,19 @@ import { http } from "viem";
 import { GuestbookAbi } from "./abis/GuestbookAbi";
 
 export default createConfig({
-  networks: {
-    arbitrum: {
-      chainId: 42161,
-      transport: http(process.env.PONDER_RPC_URL_42161),
- 			pollingInterval: 1_500,
-    },
-  },
-  contracts: {
-    Guestbook: {
-      abi: GuestbookAbi,
-      address: "0xF89E1749D28a5fEbB984584D795Abe17218AA2a7",
-      network: "arbitrum",
-      startBlock: 333596361
-    },
-  },
+	networks: {
+		arbitrum: {
+			chainId: 42161,
+			transport: http(process.env.PONDER_RPC_URL_42161),
+			pollingInterval: 1_500,
+		},
+	},
+	contracts: {
+		Guestbook: {
+			abi: GuestbookAbi,
+			address: "0x64db8B9EccdFeaC65cdC8c1B0F25d79431BB8B7E",
+			network: "arbitrum",
+			startBlock: 333596361,
+		},
+	},
 });
